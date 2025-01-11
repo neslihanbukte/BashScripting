38eaac888312ba2a163f652f326f6a4b91fdbb57
Neslihan Bükte <neslihanbukte20@gmail.com>
2025-01-11 18:49:39 +0300

    Scripts and Variables

---
+++ ScriptsandVariables/variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+# This comment line
+
+name = "Neslihan" 
+number = 34
+echo "Hello, $name"
+echo "Number : $number"
+
+#Get input from user
+echo "Please enter your name"
+read name
+echo "Hello, $name !"
