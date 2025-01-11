1004f442a51e8b2bdd94379e9211ee2f77483b60
Neslihan Bükte <neslihanbukte20@gmail.com>
2025-01-11 20:41:22 +0300

    Environment Variables

---
+++ EnvironmentVariables/environmentvar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Displaying environment variables with explanations
+echo "Your home directory: $HOME"  # : Specifies the user's home directory.
+echo "Your system path: $PATH"  # : It contains directories containing executable files on the system.
+echo "Your username: $USER"  # : Shows the name of the currently logged in user.
+echo "Your current directory: $PWD"  # : Shows the working directory you are currently in.
+echo "Your shell: $SHELL"  # : Specifies the shell program the user is using.
+echo "Your editor: $EDITOR"  # : Specifies the user's preferred text editor.
