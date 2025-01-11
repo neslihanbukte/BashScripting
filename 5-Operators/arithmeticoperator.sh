a88f5834daa6ddd8b482a3d4db0f5f02932049a7
Neslihan Bükte <neslihanbukte20@gmail.com>
2025-01-11 21:22:39 +0300

    Arithmetic Operators

---
+++ 5-Operators/arithmeticoperator.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+a=10
+b=2
+
+addition=$((a+b))
+echo "Addition:$addition"
+
+subtraction=$((a-b))
+echo "Subtraction:$subtraction" 
+
+multiplication=$((a*b))
+echo "Multiplication:$multiplication"
+
+division=$((a/b))
+echo "Divison:$division"
+
+mod=$((a%b))
+echo "Mod:$mod"
---
+++ VariablesExample/examplevar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Enter username:"
+read name
+echo "Enter your password:"
+read -s password
+
+if [[ $name == "Carla" && $password == "123qwe" ]]; then
+echo "Welcome,$name!"
+login=true
+else
+echo "Login failed, try again!"
+login=false
+fi
